38e2de43717f88d8d218c59f0fc946b223532630
Jonathan Headen <jonathan.headen@outlook.com>
2016-01-29 18:12:47 +1100

    added dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject reporting-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "reporting example :- tutorial"
+  :url "no-url"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [postgresql/postgresql "9.1-901.jdbc4"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [clj-pdf "1.11.6"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler reporting-example.handler/app
          :init reporting-example.handler/init
