fa73ca7c25b8b816cf8a08c16726108ee9e628b1
Jonathan Headen <jonathan.headen@outlook.com>
2016-02-01 07:07:08 +1100

    added formatting functions to reports

--- src/reporting_example/reports.clj
+++ src/reporting_example/reports.clj
@@ -13,3 +13,31 @@
    "yet another item"]
   [:paragraph "I'm a paragraph!"]]
  "doc.pdf")
+
+(def employee-template-paragraph
+  (template
+   [:paragraph
+    [:heading {:style {:size 15}} $name]
+    [:chunk {:style :bold} "occupation: "] $occupation "\n"
+    [:chunk {:style :bold} "place: "] $place "\n"
+    [:chunk {:style :bold} "country: "] $country [:spacer]]))
+
+(defn table-report [out]
+  (pdf
+   [{:header "Employee List"}
+    (into [:table
+           {:border false
+            :cell-border false
+            :header [{:color [0 150 150]} "Name" "Occupation" "Place" "Country"]}]
+          (employee-template
+           (db/read-employees)))]
+   out))
+
+(defn list-report [out]
+  (pdf
+   [{}
+    [:heading {:size 10} "Employees"] [:line]
+    [:spacer]
+    (employee-template-paragraph
+     (db/read-employees))]
+   out))
